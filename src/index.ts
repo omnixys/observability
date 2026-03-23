ec47938c9b725e7c881196b6723174c435c3269b
Caleb-Script <caleb-script@outlook.de>
2026-03-23 18:11:21 +0100

    fix(index): update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
+export * from "./context";
 export * from "./core";
-
-// curated exports (DX friendly)
-export * from "./tracing/trace.service.js";
-export * from "./tracing/span.decorator.js";
-
-export * from "./context/correlation-id.service.js";
-export * from "./context/request-context.service.js";
-
-export * from "./propagation/w3c-propagator.js";
-export * from "./propagation/kafka-propagator.js";
+export * from "./graphql";
+export * from "./http";
+export * from "./logging";
+export * from "./metrics";
+export * from "./propagation";
+export * from "./tracing";
